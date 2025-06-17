4db5ff50bcc9c6c2c9057ae358b2c48bfeea70a4
IndraNaik <pathlavath.indra@gmail.com>
2025-06-17 05:29:33 -0700

    day1

---
+++ hello.rb
@@ -0,0 +1,28 @@
+  puts "Hello World"
+
+  puts "Hello Again"
+
+  # This is comment line
+
+=begin
+    This is multi
+    comment in ruby
+=end
+
+puts "Indra has #{25+30/6} Rupees in bank account"
+
+# variables:
+
+cars = 0
+bike = 1
+
+puts "Indra has #{cars} cars and #{bike} bike"
+
+print "How old are you: "
+age = gets.chomp
+print "How tall are you: "
+height = gets.chomp
+puts "Indra is #{age} years old and #{height} cms."
+print "How old are you: "
+num = gets.chomp.to_f
+puts "Naik is #{num} years old."
---
+++ numbers.rb
@@ -0,0 +1,12 @@
+#integers
+puts 1 + 2
+
+# floating
+puts 1 + 3.2
+
+# Mul and Sub
+puts 3.2 - 2
+puts 3.4 * 4
+
+#Exponential
+puts 2 ** 4
