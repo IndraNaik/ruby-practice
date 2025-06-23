f2eea760b84a3a59841af9e0f5cff9a6f62182e7
IndraNaik <pathlavath.indra@gmail.com>
2025-06-23 16:30:18 +0530

    strings

---
+++ Day-3/string.rb
@@ -0,0 +1,24 @@
+'This is a simple Ruby string literal'
+puts 'This \ is \ a \ simple \ Ruby \string \ literal'
+
+# Expression Substitution
+name = 'Indra'
+puts "Hello #{name}!"
+
+# String Concatenation
+greeting = 'Hello'
+greeting += ' World!'
+puts greeting
+
+# String Interpolation
+age = 25
+puts "I am #{age} years old."
+
+x, y, z = 1, 2, 3
+puts "The values are: #{x}, #{y}, and #{z}."
+puts "The sum of x and y is: #{x + y}."
+puts "The sum of x, y, and z is: #{x + y + z}."
+puts "The difference between x and y is: #{x - y}."
+puts "The difference between z and y is: #{z - y}."
+puts "The product of x and y is: #{x * y}."
+puts "The product of x, y, and z is: #{x * y * z}."
